758620b66989f328e4319129a57ea743c771aaaf
João Igor <104221138+JoaoPC10@users.noreply.github.com>
2025-03-13 21:41:21 -0300

    Add files via upload

---
+++ clientes/src/main/java/io/github/dougllasfps/clientes/model/reporitory/ServicoRepository.java
@@ -0,0 +1,6 @@
+package io.github.dougllasfps.clientes.model.reporitory;
+import io.github.dougllasfps.clientes.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
